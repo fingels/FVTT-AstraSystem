c47887a430eae767c23de8d863bb410b717fb09f
Florian Ingels <florian.ingels@gmail.com>
2026-03-13 12:12:44 +0100

    Modified size of chat images

--- modules/die.js
+++ modules/die.js
@@ -19,12 +19,12 @@ export class DieAstra extends Die {
     /** @override */
     getResultLabel(result) {
         return {
-			"1": '<img src="modules/astra-system/images/empty.png" />',
-            "2": '<img src="modules/astra-system/images/empty.png" />',
-            "3": '<img src="modules/astra-system/images/HalfStar.png" />',
-            "4": '<img src="modules/astra-system/images/HalfStar.png" />',
-			"5": '<img src="modules/astra-system/images/Star.png" />',
-            "6": '<img src="modules/astra-system/images/Star.png" />'
+			"1": '<img src="modules/astra-system/images/Empty_chat.png" />',
+            "2": '<img src="modules/astra-system/images/Empty_chat.png" />',
+            "3": '<img src="modules/astra-system/images/HalfStar_chat.png" />',
+            "4": '<img src="modules/astra-system/images/HalfStar_chat.png" />',
+			"5": '<img src="modules/astra-system/images/Star_chat.png" />',
+            "6": '<img src="modules/astra-system/images/Star_chat.png" />'
         }[result.result];
     }
 }
